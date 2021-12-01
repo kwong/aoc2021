5f0788e24a35ab70e53cfa338e6bba7020b0a8fb
Kangwei Wong <kangwei.wong@servian.com>
2021-12-01 20:34:22 +0800

    Format code

--- 1/solve.py
+++ 1/solve.py
@@ -1,22 +1,22 @@
 def sliding_window_list(data, s=3):
     new_list = []
-    for i in range(0, len(data)-2):
-        new_list += [data[slice(i, i+s)]]
+    for i in range(0, len(data) - 2):
+        new_list += [data[slice(i, i + s)]]
     return new_list
 
 
 def sum_lists(data):
     new_list = []
     for r in data:
-        #print(r)
+        # print(r)
         new_list += [sum(r)]
-
     return new_list
+
 
 def count_positive_increment(data):
     count = 0
     for i in range(1, len(data)):
-        if data[i] > data[i-1]:
+        if data[i] > data[i - 1]:
             count += 1
     return count
 
@@ -24,12 +24,10 @@ def count_positive_increment(data):
 with open("./input.txt", "r") as f:
     content = f.readlines()
     data = [int(i) for i in content]
-
-    #A
+    # A
     print(count_positive_increment(data))
 
-    #B
+    # B
     list_1 = sliding_window_list(data)
     list_2 = sum_lists(list_1)
     print(count_positive_increment(list_2))
-
